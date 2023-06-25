7179611120796e4c193a919df4fc4256a667e5c6
sabbir-shuvo-ux <shuvosabbir134@gmail.com>
2023-06-25 15:52:04 +0600

    added weather forecaste for next 7days and update error component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,24 @@
 import React from "react";
-import { testIcon } from "../assets/index";
+import moment from "moment";
 
-const Card = () => {
+const Card = ({ item }) => {
+  const itemDate = item?.valid_date;
+  const dayName = moment(itemDate).format("dddd");
+
   return (
-    <div className="card">
-      <h2 className="text-white uppercase">sunday</h2>
-      <img className="mx-auto w-[50px]" src={testIcon} alt="test img" />
-      <h3 className="text-white uppercase">Broken clouds</h3>
-      <h4 className="text-white uppercase text-[1.25rem]">20°</h4>
+    <div className="card cursor-pointer">
+      <h2 className="text-white uppercase text-[14px]">
+        {" "}
+        {item?.valid_date ? dayName : null}{" "}
+      </h2>
+      <img
+        className="mx-auto w-[50px]"
+        src={`https://cdn.weatherbit.io/static/img/icons/${item?.weather?.icon}.png`}
+        alt={item?.weather?.description}
+        title={item?.weather?.description}
+      />
+      <h3 className="text-white uppercase"> {item?.weather?.description} </h3>
+      <h4 className="text-white uppercase text-[1.25rem]"> {item?.temp}°</h4>
     </div>
   );
 };
